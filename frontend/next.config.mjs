2039149b86bc02f0d769b6479143d4c7c8f799bd
Andres Castiglia <acastiglia@gmail.com>
2025-12-11 15:07:25 -0300

    feat: add Content-Security-Policy header to Next.js configuration.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,6 +7,19 @@ const nextConfig = {
     images: {
         domains: ['localhost', 'rent.maese.com.ar'],
     },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "script-src 'self' 'unsafe-eval' 'unsafe-inline'; object-src 'none';",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default withNextIntl(nextConfig);
