e7b4d231b29a58e81ef828bdb8e8ff64c5de4896
Andres Castiglia <acastiglia@gmail.com>
2026-06-25 17:14:05 -0300

    chore: update dependencies and devDependencies in mobile package.json
    - Updated various dependencies to their latest versions for improved performance and security.
    - Adjusted the version of Expo and related packages to align with the latest Expo SDK.
    - Updated TypeScript and ESLint configurations for better compatibility and features.
    
    refactor: enhance TypeScript configuration
    
    - Modified tsconfig.json to include additional paths and exclude test files.
    - Introduced a new tsconfig.spec.json for better handling of test files with Jest.
    
    chore: add Node engine requirement in root package.json
    
    - Specified minimum Node version requirement to ensure compatibility across environments.

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     '^.+\\.(ts|tsx)$': [
       'ts-jest',
       {
-        tsconfig: '<rootDir>/tsconfig.json',
+        tsconfig: '<rootDir>/tsconfig.spec.json',
       },
     ],
   },
